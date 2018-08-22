ee6c1d7fdbfb0b48e44a98f5c8dfa5604262f421
Crystal Nieves <cnieves90@Gmail.com>
2018-08-22 09:19:49 -0400

    Fixed bug on replay modal

--- js/app.js
+++ js/app.js
@@ -110,6 +110,7 @@ function checkForMatch() {
         toggledCards = [];
         matched++;
         if (matched === TOTAL_PAIRS) {
+            matched = 0;
             gameOver();
         }
     } else {
@@ -238,5 +239,4 @@ function cardsReset() {
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)*/
